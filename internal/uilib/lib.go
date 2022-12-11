72009b22da8d6296d9b79429b5ccfcbd25057f4f
gauravsharma <gaurav.sharma@devrev.ai>
2022-12-11 20:02:00 +0530

    http test

--- internal/uilib/lib.go
+++ internal/uilib/lib.go
@@ -1,5 +1,28 @@
 package uilib
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
 func Name() string {
 	return "Hello World!!"
+}
+
+func testHttp() string {
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+	if err != nil {
+		fmt.Println(err)
+	}
+	//We Read the response body on the line below.
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//Convert the body to type string
+	sb := string(body)
+	fmt.Println(sb)
+	return sb
 }
